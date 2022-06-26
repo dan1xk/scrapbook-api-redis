6a0365613d941a72ac9a19a79420546455b83fe7
Dann <sdanirobert@gmail.com>
2022-06-25 21:22:58 -0300

    add config tests

--- ormconfig.js
+++ ormconfig.js
@@ -1,22 +1,29 @@
 require('dotenv').config();
 
-module.exports = {
-    type: process.env.DATABASE_TYPE,
-    url: process.env.DATABASE_URL,
-    logging: false,
-    extra: {
-        ssl: {
-            rejectUnauthorized: false
+let config = {};
+
+if (process.env.NODE_ENV?.toLocaleLowerCase() === 'test') {
+    config = {
+        type: 'sqlite',
+        database: './db-test.sql',
+        migrations: [ 'src/database/migrations/**/*' ],
+        entities: [ 'src/database/entities/**/*' ]
+    };
+} else {
+    config = {
+        type: process.env.DATABASE_TYPE,
+        url: process.env.DATABASE_URL,
+        logging: false,
+        extra: {
+            ssl: { rejectUnauthorized: false }
+        },
+        migrations: [ 'src/database/migrations/**/*' ],
+        entities: [ 'src/database/entities/**/*' ],
+        cli: {
+            entitiesDir: 'src/database/entities',
+            migrationsDir: 'src/database/migrations'
         }
-    },
-    migrations: [
-        'src/database/migrations/**/*'
-    ],
-    entities: [
-        'src/database/entities/**/*'
-    ],
-    cli: {
-        entitiesDir: 'src/database/entities',
-        migrationsDir: 'src/database/migrations'
-    }
-};
+    };
+}
+
+module.exports = config
